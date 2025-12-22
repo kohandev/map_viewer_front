7b1a22992fe24e75e4fe804d31b889ccb10ff970
kohan.dev <kohan.dev@gmail.com>
2025-12-22 15:33:22 +0200

    add start lat and lng

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
-// export const POINT_DELETE_TIME = 5 * 60 * 1000; // 5 minutes
-// export const POINT_LOSS_TIME = 60 * 1000; // 1 minute
-export const POINT_DELETE_TIME = 15 * 1000; // 5 minutes
-export const POINT_LOSS_TIME = 5 * 1000; // 1 minute
+export const POINT_DELETE_TIME = 5 * 60 * 1000; // 5 minutes
+export const POINT_LOSS_TIME = 60 * 1000; // 1 minute
+
+export const START_LAT = 47.73;
+export const START_LNG = 37.67;
--- src/pages/MainPage/components/GeoMap/GeoMap.tsx
+++ src/pages/MainPage/components/GeoMap/GeoMap.tsx
@@ -2,12 +2,17 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 import { GeoMarker } from '../GeoMarker/GeoMarker.tsx';
 import geoStore from '../../../../store/geo-store.ts';
 import { observer } from 'mobx-react-lite';
+import { START_LAT, START_LNG } from '../../../../constants.ts';
 
 export const GeoMap = observer(() => {
   const { geoPoints } = geoStore;
 
   return (
-    <MapContainer center={[50.45, 30.52]} zoom={6} style={{ height: '100%', width: '100%' }}>
+    <MapContainer
+      center={[START_LAT, START_LNG]}
+      zoom={12}
+      style={{ height: '100%', width: '100%' }}
+    >
       <TileLayer
         attribution="&copy; OpenStreetMap contributors"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
